48fc6a75f880c7e6e95e6f085d8777e61876797d
Misuzu Taniguchi <me.tpn23@gmail.com>
2023-11-04 16:33:42 -0700

    Modify hospital details page

---
+++ scripts/hospital_detail.js
@@ -0,0 +1,22 @@
+function displayHospitalInfo() {
+    let params = new URL(window.location.href); //get URL of search bar
+    console.log("param is =", params)
+    let ID = params.searchParams.get("docID"); //get value for key "id"
+    console.log(ID);
+
+    // doublecheck: is your collection called "Reviews" or "reviews"?
+    db.collection("hospitals")
+        .doc(ID)
+        .get()
+        .then(doc => {
+            thisHospital = doc.data();
+            hospitalCode = thisHospital.code;
+            hospitalName = doc.data().name;
+
+            // only populate title, and image
+            document.getElementById("hospitalName").innerHTML = hospitalName;
+            let imgEvent = document.querySelector(".hospital-img");
+            imgEvent.src = "../images/" + hospitalCode + ".png";
+        });
+}
+displayHospitalInfo();
