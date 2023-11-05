426da98eb7d91ded68ab743c0902255e5e4d2e78
Hsin Pang <hpang19@my.bcit.ca>
2023-11-05 15:36:43 -0800

    update user profile js

--- scripts/user_profile.js
+++ scripts/user_profile.js
@@ -3,6 +3,9 @@ function writeProfile() {
     
     var user_profile = db.collection('userProfiles').doc(user.uid);
     var user_profile_extension = user_profile.collection('extension');
+    var tobacco = document.getElementsByName('tobacco');
+    var alcohol = document.getElementsByName('alcohol');
+    
     user_profile.set({
         first_name: $("#exampleInputFname").val(),
         last_name: $("#exampleInputLname").val(),
@@ -16,11 +19,23 @@ function writeProfile() {
         city: $("#exampleInputCity").val(),
         province: $("#exampleInputProvince").val(),
         postal_code: $("#exampleInputPostalCode").val(),
-        
-        // date_of_birth: $("#").val(),
-        // date_of_birth: $("#").val(),
-        // date_of_birth: $("#").val(),
     }).then(function () {
+        for (i = 0; i < tobacco.length; i++) {
+            if (tobacco[i].checked) {
+                tobacco_val = tobacco[i].getAttribute('id')
+                break;
+            }
+        }
+        for (i = 0; i < alcohol.length; i++) {
+            if (alcohol[i].checked) {
+                alcohol_val = alcohol[i].getAttribute('id')
+                break;
+            }
+        }
+        user_profile_extension.doc('smoke_and_drink').set({
+            tobacco: tobacco_val,
+            alcohol: alcohol_val
+        })
         if ($("#exampleInputEmergengyName").val()) {
             user_profile_extension.doc('emergency_contact').set({
                 name: $("#exampleInputEmergengyName").val(),
@@ -35,9 +50,21 @@ function writeProfile() {
                 phone: $("#exampleInputDoctorPhone").val(),
             })
         }
+        if ($("#exampleInputMedications").val()) {
+            user_profile_extension.doc('medical_history').set({
+                medication: $("#exampleInputMedications").val(),
+                allergy: $("#exampleInputAllergies").val(),
+                surgery: $("#exampleInputSurgery").val(),
+            })
+        }
+        if ($("#exampleInputAdditional").val()) {
+            user_profile_extension.doc('additional').set({
+                info: $("#exampleInputAdditional").val()
+            })
+        }
     }).then(function () {
         console.log("user profile updated");
-        window.location.assign("confirm_registration.html");
+        // window.location.assign("confirm_registration.html");
     }).catch(function (error) {
         console.log("Error adding user registration: " + error);
     });
