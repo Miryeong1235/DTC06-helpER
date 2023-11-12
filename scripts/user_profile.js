1dd74c38b7f5a81ce83aec133a341b26a4f7ed5f
Hsin Pang <hpang19@my.bcit.ca>
2023-11-12 01:19:16 -0800

    enable autofill personal info from database in registration page

--- scripts/user_profile.js
+++ scripts/user_profile.js
@@ -1,3 +1,8 @@
+function populateUserInfo() {
+    console.log('populate user info from firestore database');
+    readProfile(true);
+}
+
 function writeProfile() {
     var user = firebase.auth().currentUser;
 
@@ -6,6 +11,7 @@ function writeProfile() {
     var tobacco = document.getElementsByName('tobacco');
     var alcohol = document.getElementsByName('alcohol');
 
+    // write basic info
     user_profile.set({
         first_name: $("#exampleInputFname").val(),
         last_name: $("#exampleInputLname").val(),
@@ -18,153 +24,181 @@ function writeProfile() {
         city: $("#exampleInputCity").val(),
         province: $("#exampleInputProvince").val(),
         postal_code: $("#exampleInputPostalCode").val(),
-    }).then(function () {
-        for (i = 0; i < tobacco.length; i++) {
-            if (tobacco[i].checked) {
-                tobacco_id = tobacco[i].getAttribute('id');
-                tobacco_val = $(`#${tobacco_id}Label`).text();
-                break;
-            }
+    });
+
+    // retrieve smoking frequency
+    for (i = 0; i < tobacco.length; i++) {
+        if (tobacco[i].checked) {
+            tobacco_id = tobacco[i].getAttribute('id');
+            tobacco_val = $(`#${tobacco_id}Label`).text();
+            break;
         }
-        for (i = 0; i < alcohol.length; i++) {
-            if (alcohol[i].checked) {
-                alcohol_id = alcohol[i].getAttribute('id');
-                alcohol_val = $(`#${alcohol_id}Label`).text();
-                break;
-            }
+    };
+
+    // retrieve drinking frequency
+    for (i = 0; i < alcohol.length; i++) {
+        if (alcohol[i].checked) {
+            alcohol_id = alcohol[i].getAttribute('id');
+            alcohol_val = $(`#${alcohol_id}Label`).text();
+            break;
         }
-        user_profile_extension.doc('smoke_and_drink').set({
-            tobacco: tobacco_val,
-            alcohol: alcohol_val
+    };
+
+    // write emergency contact
+    console.log($("#exampleInputEmergengyName").val(), 'outside emergency')
+    if ($("#exampleInputEmergengyName").val()) {
+        console.log('inside the if for emergency')
+        user_profile_extension.doc('emergency_contact').set({
+            name: $("#exampleInputEmergengyName").val(),
+            phone: $("#exampleInputEmergengyPhone").val(),
+            relationship: $("#exampleInputRelation").val(),
         })
-        console.log($("#exampleInputEmergengyName").val(), 'outside emergency')
-        if ($("#exampleInputEmergengyName").val()) {
-            console.log('inside the if for emergency')
-            user_profile_extension.doc('emergency_contact').set({
-                name: $("#exampleInputEmergengyName").val(),
-                phone: $("#exampleInputEmergengyPhone").val(),
-                relationship: $("#exampleInputRelation").val(),
-            })
-        }
-        if ($("#exampleInputFamilyDoctorName").val()) {
-            user_profile_extension.doc('family_doctor').set({
-                name: $("#exampleInputFamilyDoctorName").val(),
-                clinic: $("#exampleInputFamilyClinicName").val(),
-                phone: $("#exampleInputDoctorPhone").val(),
-            })
-        }
-        if ($("#exampleInputMedications").val()) {
-            user_profile_extension.doc('medical_history').set({
-                medication: $("#exampleInputMedications").val(),
-                allergy: $("#exampleInputAllergies").val(),
-                surgery: $("#exampleInputSurgery").val(),
-            })
-        }
-        if ($("#exampleInputAdditional").val()) {
-            user_profile_extension.doc('additional').set({
-                info: $("#exampleInputAdditional").val()
-            })
-        }
-    }).then(function () {
+    };
+
+    // write family doctor
+    if ($("#exampleInputFamilyDoctorName").val()) {
+        user_profile_extension.doc('family_doctor').set({
+            name: $("#exampleInputFamilyDoctorName").val(),
+            clinic: $("#exampleInputFamilyClinicName").val(),
+            phone: $("#exampleInputDoctorPhone").val(),
+        })
+    };
+
+    // write medical history
+    if ($("#exampleInputMedications").val()) {
+        user_profile_extension.doc('medical_history').set({
+            medication: $("#exampleInputMedications").val(),
+            allergy: $("#exampleInputAllergies").val(),
+            surgery: $("#exampleInputSurgery").val(),
+        })
+    };
+
+    // write additional info
+    if ($("#exampleInputAdditional").val()) {
+        user_profile_extension.doc('additional').set({
+            info: $("#exampleInputAdditional").val()
+        })
+    };
+
+    // write drinking and smoking frequency
+    user_profile_extension.doc('smoke_and_drink').set({
+        tobacco: tobacco_val,
+        alcohol: alcohol_val
+    }).then(() => {
         console.log("user profile updated");
-    }).then(function() {
+    }).then(() => {
         window.location.assign("confirm_registration.html");
-    }).catch(function (error) {
+    }).catch((error) => {
         console.log("Error adding user registration: " + error);
     });
 }
 
-function readProfile() {
+function readProfile(autofillRegistration = false) {
     firebase.auth().onAuthStateChanged(user => {
 
         var user_profile = db.collection('userProfiles').doc(user.uid);
         var user_profile_extension = user_profile.collection('extension');
-        var tobacco = undefined; // document.getElementsByName('tobacco');
-        var alcohol = undefined; // document.getElementsByName('alcohol');
 
         user_profile.get().then(profile => {
             let data = profile.data();
             if (data) {
-                $("#exampleInputFname").text(data.first_name);
-                $("#exampleInputLname").text(data.last_name);
-                $("#exampleInputEmail1").text(data.email);
-                $("#exampleInputdof").text(data.date_of_birth);
-                $("#exampleInputPhone").text(data.phone);
-                $("#exampleInputPHN").text(data.phn);
-                $("#exampleInputStreetNumber").text(data.street_no);
-                $("#exampleInputStreetName").text(data.street_name);
-                $("#exampleInputCity").text(data.city);
-                $("#exampleInputProvince").text(data.province);
-                $("#exampleInputPostalCode").text(data.postal_code);
+                let elementIds = ['exampleInputFname', 'exampleInputLname', 'exampleInputEmail1', 'exampleInputdof', 'exampleInputPhone',
+                    'exampleInputPHN', 'exampleInputStreetNumber', 'exampleInputStreetName', 'exampleInputCity', 'exampleInputProvince', 
+                    'exampleInputPostalCode'];
+                let attrNames = ['first_name', 'last_name', 'email', 'date_of_birth', 'phone', 'phn', 'street_no', 'street_name', 'city', 
+                    'province', 'postal_code']
+                for (i = 0; i < elementIds.length; i++) {
+                    let elementId = elementIds[i];
+                    if (autofillRegistration) {
+                        $(`#${elementId}`).val(data[attrNames[i]]);
+                    } else {
+                        $(`#${elementId}`).text(data[attrNames[i]]);
+                    }
+                }}
             }
-        })
+        )
 
         user_profile_extension.doc('emergency_contact').get().then(emergency => {
             let data = emergency.data();
             if (data) {
-                $("#exampleInputEmergengyName").text(data.name);
-                $("#exampleInputEmergengyPhone").text(data.phone);
-                $("#exampleInputRelation").text(data.relationship);
+                let elementIds = ['exampleInputEmergengyName', 'exampleInputEmergengyPhone', 'exampleInputRelation'];
+                let attrNames = ['name', 'phone', 'email', 'relationship']
+                for (i = 0; i < elementIds.length; i++) {
+                    let elementId = elementIds[i];
+                    if (autofillRegistration) {
+                        $(`#${elementId}`).val(data[attrNames[i]]);
+                    } else {
+                        $(`#${elementId}`).text(data[attrNames[i]]);
+                    }
+                }
             }
         })
 
         user_profile_extension.doc('medical_history').get().then(medical => {
             let data = medical.data();
             if (data) {
-                $("#exampleInputMedications").text(data.medication);
-                $("#exampleInputAllergies").text(data.allergy);
-                $("#exampleInputSurgery").text(data.surgery);
+                let elementIds = ['exampleInputMedications', 'exampleInputAllergies', 'exampleInputSurgery'];
+                let attrNames = ['medication', 'allergy', 'surgery']
+                for (i = 0; i < elementIds.length; i++) {
+                    let elementId = elementIds[i];
+                    if (autofillRegistration) {
+                        $(`#${elementId}`).val(data[attrNames[i]]);
+                    } else {
+                        $(`#${elementId}`).text(data[attrNames[i]]);
+                    }
+                }
             }
         })
 
         user_profile_extension.doc('family_doctor').get().then(doctor => {
             let data = doctor.data();
             if (data) {
-                $("#exampleInputFamilyDoctorName").text(data.name);
-                $("#exampleInputFamilyClinicName").text(data.clinic);
-                $("#exampleInputDoctorPhone").text(data.phone);
+                let elementIds = ['exampleInputFamilyDoctorName', 'exampleInputFamilyClinicName', 'exampleInputDoctorPhone'];
+                let attrNames = ['name', 'clinic', 'phone']
+                for (i = 0; i < elementIds.length; i++) {
+                    let elementId = elementIds[i];
+                    if (autofillRegistration) {
+                        $(`#${elementId}`).val(data[attrNames[i]]);
+                    } else {
+                        $(`#${elementId}`).text(data[attrNames[i]]);
+                    }
+                }
             }
         })
 
         user_profile_extension.doc('smoke_and_drink').get().then(alcohol_cigarette => {
             let data = alcohol_cigarette.data();
             if (data) {
-                $("#exampleInputAlcohol").text(data.alcohol);
-                $("#exampleInputCigarette").text(data.tobacco);
+                if (!autofillRegistration) {
+                    $("#exampleInputAlcohol").text(data.alcohol);
+                    $("#exampleInputCigarette").text(data.tobacco);
+                } else {
+                    let formControl = document.getElementsByClassName('form-check-label');
+
+                    // loop through elements in form-control classes
+                    for (i = 0; i < formControl.length; i++) {
+                        labelElementId = formControl[i].id;
+                        inputElementId = labelElementId.substring(0, labelElementId.indexOf('Label'));
+                        inputElementInnerHTML = $(`#${labelElementId}`).text().trim();
+                        console.log(1, data.alcohol.trim(), data.tobacco.trim(), 2, inputElementInnerHTML)
+                        if ([data.alcohol.trim(), data.tobacco.trim()].includes(inputElementInnerHTML)) {
+                            $(`#${inputElementId}`)[0].checked = true;
+                        } else {
+                            $(`#${inputElementId}`)[0].checked = false;
+                        };
+                    }
+                }
             }
-        })
+        });
 
         user_profile_extension.doc('additional').get().then(addition => {
             if (addition.data()) {
-                $("#exampleInputAdditional").text(addition.data().info);
+                if (autofillRegistration) {
+                    $("#exampleInputAdditional").val(addition.data().info);
+                } else {
+                    $("#exampleInputAdditional").text(addition.data().info);
+                }
             }
         })
-
-
-        // user_profile_extension.doc('additional').data().info);
-        // })
-        // .then(function () {
-        //     for (i = 0; i < tobacco.length; i++) {
-        //         if (tobacco[i].checked) {
-        //             tobacco_val = tobacco[i].getAttribute('id')
-        //             break;
-        //         }
-        //     }
-        //     for (i = 0; i < alcohol.length; i++) {
-        //         if (alcohol[i].checked) {
-        //             alcohol_val = alcohol[i].getAttribute('id')
-        //             break;
-        //         }
-        //     }
-        //     user_profile_extension.doc('smoke_and_drink').set({
-        //         tobacco: tobacco_val,
-        //         alcohol: alcohol_val
-        //     })
-        // }).then(function () {
-        //     console.log("user profile updated");
-        //     window.location.assign("confirm_registration.html");
-        // }).catch(function (error) {
-        //     console.log("Error adding user registration: " + error);
-        // });
     })
 }
