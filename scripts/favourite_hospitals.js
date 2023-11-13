1b6cd5a287bae8b7a229eea4192be28840f27a00
Misuzu Taniguchi <me.tpn23@gmail.com>
2023-11-12 18:42:31 -0800

    Add function to change colour of icon

--- scripts/favourite_hospitals.js
+++ scripts/favourite_hospitals.js
@@ -1,32 +1,58 @@
-const favouriteDocRef = db.collection('favorites').doc('user1'); // user1はユーザーIDなどに置き換えてください
+
+var favouriteDocRef = undefined;
+var hospitalIdList = undefined;
 
 // お気に入りの状態を管理する変数
-let isFavorite = false;
+var userUid = undefined;
+
+function getUserId() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            userUid = user.uid;
+            favouriteDocRef = db.collection('userProfiles').doc(userUid).collection('favourite');
+        } else {
+            console.log('User not logged in')
+        }
+    })
+}
 
 // お気に入りの状態を切り替える関数
-function toggleFavorite() {
-    if (isFavorite) {
-        removeFromFavorites();
-    } else {
-        addToFavorites();
+function toggleFavourite(elem) {
+    let url = $(elem).siblings('a').attr('href');
+    let hospitalId = url.split('?docID=')[1];
+    if (favouriteDocRef) {
+        favouriteDocRef.get().then(querySnapshot => {
+            return querySnapshot.docs.map(doc => doc.id);
+        }).then(hospitalIdList => {
+            console.log(hospitalIdList);
+            if (hospitalIdList.includes(hospitalId)) {
+                removeFromFavourites(hospitalId);
+                console.log('invoke remove function to remove hospital from favourite list');
+            } else {
+                addToFavourites(hospitalId);
+                console.log('invoke add function to add hospital to favourite list');
+            }
+        })
     }
 }
 
 // お気に入りに追加する関数
-function addToFavorites() {
-    favouriteDocRef.set({ favorite: true })
-        .then(() => {
-            console.log("Document successfully written!");
-            updateHeartColor(true);
-        })
+function addToFavourites(hospitalId) {
+    favouriteDocRef.doc(hospitalId).set({
+        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+    }).then(() => {
+        console.log("Document successfully written!");
+        updateHeartColor(true);
+    })
         .catch((error) => {
             console.error("Error writing document: ", error);
         });
 }
 
-// Delete from Favorites
-function removeFromFavorites() {
-    favouriteDocRef.delete()
+// Delete from Favourites
+function removeFromFavourites(hospitalId) {
+    favouriteDocRef.doc(hospitalId).delete()
         .then(() => {
             console.log("Document successfully deleted!");
             updateHeartColor(false);
@@ -37,21 +63,22 @@ function removeFromFavorites() {
 }
 
 // Change the color 
-function updateHeartColor(isFavourited) {
-    isFavourite = isFavourited;
-    const heartElement = document.getElementById('heart');
-    heartElement.innerHTML =
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-        </svg>;
+function updateHeartColor(fill) {
+    let heartElement = document.getElementById('heart');
+    if (fill) {
+        heartElement.innerHTML =
+            `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+            </svg>`;
+    } else {
+        heartElement.innerHTML =
+            `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-heart" viewBox="0 0 16 16">
+                <path
+                    d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+            </svg>`;
+    }
+
 }
 
-// お気に入りの状態を監視し、初期表示を設定
-favouriteDocRef.onSnapshot((doc) => {
-    const data = doc.data();
-    if (data && data.favourite) {
-        updateHeartColor(true);
-    } else {
-        updateHeartColor(false);
-    }
-});
+getUserId();
--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
+var userUid = undefined;
+
 function getNameFromAuth() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
         if (user) {
             // Do something for the currently logged-in user here: 
+            userUid = user.uid;
             console.log(user.uid); //print the uid in the browser console
             console.log(user.displayName);  //print the user name in the browser console
             userName = user.displayName;
@@ -97,6 +100,28 @@ function writeHospitals() {
     });
 }
 
+function fillHeart(elem, hospitalId) {
+    let element = elem.querySelector('#heart');
+    let favouriteDocRef = db.collection('userProfiles').doc(userUid).collection('favourite');
+    if (favouriteDocRef) {
+        favouriteDocRef.get().then(querySnapshot => {
+            return querySnapshot.docs.map(doc => doc.id);
+        }).then(hospitalIdList => {
+            if (hospitalIdList.includes(hospitalId)) {
+                element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+            </svg>`;
+            } else {
+                element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-heart" viewBox="0 0 16 16">
+                <path
+                    d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+            </svg>`;
+            }
+        })
+    }
+}
+
 //------------------------------------------------------------------------------
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
@@ -115,6 +140,7 @@ function displayCardsDynamically(collection) {
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
                 //update title and text and image
+                fillHeart(newcard, docID);
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-hour').innerHTML = hospitalHour;
                 newcard.querySelector('.card-text').innerHTML = details;
