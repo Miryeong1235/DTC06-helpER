7e50ad1be625c4295ede97031820e02f39e66cc6
Misuzu Taniguchi <me.tpn23@gmail.com>
2023-11-12 22:04:49 -0800

    Add comments

--- scripts/favourite_hospitals.js
+++ scripts/favourite_hospitals.js
@@ -2,7 +2,6 @@
 var favouriteDocRef = undefined;
 var hospitalIdList = undefined;
 
-// お気に入りの状態を管理する変数
 var userUid = undefined;
 
 function getUserId() {
@@ -17,7 +16,7 @@ function getUserId() {
     })
 }
 
-// お気に入りの状態を切り替える関数
+// toggle favourites
 function toggleFavourite(elem) {
     let url = $(elem).siblings('a').attr('href');
     let hospitalId = url.split('?docID=')[1];
@@ -37,7 +36,7 @@ function toggleFavourite(elem) {
     }
 }
 
-// お気に入りに追加する関数
+// Add to favourite
 function addToFavourites(hospitalId) {
     favouriteDocRef.doc(hospitalId).set({
         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
@@ -82,3 +81,36 @@ function updateHeartColor(fill) {
 }
 
 getUserId();
+
+
+//------------------------------------------------------------------------------
+// Input parameter is a string representing the collection we are reading from
+//------------------------------------------------------------------------------
+function displayCardsDynamically(collection, hospitalId) {
+    db.collection("hospitals").doc(hospitalId).get().then(doc => {
+        hospitalName = doc.data().name;
+    })
+
+    let cardTemplate = document.getElementById("reviewTemplate"); // Retrieve the HTML element with the ID "hospitalCardTemplate" and store it in the cardTemplate variable. 
+
+    db.collection(collection).get()   //the collection called "hikes"
+        .then(allReviews => {
+            allReviews.forEach(doc => { //iterate thru each doc
+                let doc_id = doc.id
+                if (doc_id.includes('-' + hospitalId)) {
+                    var title = hospitalName;
+                    var rating = parseInt(doc.data().rating.split(' stars')[0]);
+                    var stars = '★'.repeat(rating) + '☆'.repeat(5 - rating);
+                    var comments = doc.data().comment;
+                    let newcard = cardTemplate.content.cloneNode(true);
+                    //update title and text and image
+                    newcard.querySelector('#reviewHospitalName').innerHTML = title;
+                    newcard.querySelector('#reviewStar').innerHTML = stars;
+                    newcard.querySelector('#reviewContent').innerHTML = comments;
+
+                    document.getElementById(collection + "-go-here").appendChild(newcard);
+                }
+
+            })
+        })
+}
