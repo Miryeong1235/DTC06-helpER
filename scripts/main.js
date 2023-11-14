2ee31e12f3b4da521baac0da489a45272f823fe6
Hsin Pang <hpang19@my.bcit.ca>
2023-11-14 14:37:55 -0800

    debug heart

--- scripts/main.js
+++ scripts/main.js
@@ -100,27 +100,27 @@ function writeHospitals() {
     });
 }
 
-function fillHeart(elem, hospitalId) {
-    let element = elem.querySelector('#heart');
-    let favouriteDocRef = db.collection('userProfiles').doc(userUid).collection('favourite');
-    if (favouriteDocRef) {
-        favouriteDocRef.get().then(querySnapshot => {
-            return querySnapshot.docs.map(doc => doc.id);
-        }).then(hospitalIdList => {
-            if (hospitalIdList.includes(hospitalId)) {
-                element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
-            </svg>`;
-            } else {
-                element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-heart" viewBox="0 0 16 16">
-                <path
-                    d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-            </svg>`;
-            }
-        })
-    }
-}
+// function fillHeart(elem, hospitalId) {
+//     let element = elem.querySelector('#heart');
+//     let favouriteDocRef = db.collection('userProfiles').doc(userUid).collection('favourite');
+//     if (favouriteDocRef) {
+//         favouriteDocRef.get().then(querySnapshot => {
+//             return querySnapshot.docs.map(doc => doc.id);
+//         }).then(hospitalIdList => {
+//             if (hospitalIdList.includes(hospitalId)) {
+//                 element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+//                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+//             </svg>`;
+//             } else {
+//                 element.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+//                 class="bi bi-heart" viewBox="0 0 16 16">
+//                 <path
+//                     d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+//             </svg>`;
+//             }
+//         })
+//     }
+// }
 
 //------------------------------------------------------------------------------
 // Input parameter is a string representing the collection we are reading from
@@ -138,14 +138,16 @@ function displayCardsDynamically(collection) {
                 var hospitalHour = doc.data().hours; //gets the length field
                 var docID = doc.id;
                 let newcard = cardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
-
+                
                 //update title and text and image
-                fillHeart(newcard, docID);
+                // newcard.querySelector('#heart').innerHTML = 'abc';
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-hour').innerHTML = hospitalHour;
                 newcard.querySelector('.card-text').innerHTML = details;
                 newcard.querySelector('.card-image').src = `./images/${hospitalCode}.png`; //Example: MSJ.png
                 newcard.querySelector('a').href = "hospital_detail.html?docID=" + docID;
+                newcard.querySelector('i').id = "heart-"+docID
+                newcard.querySelector('i').onclick = ()=> updateHeart(docID)
                 //Optional: give unique ids to all elements for future use
                 // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
                 // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
@@ -153,7 +155,7 @@ function displayCardsDynamically(collection) {
 
                 //attach to gallery, Example: "hikes-go-here"
                 document.getElementById(collection + "-go-here").appendChild(newcard);
-
+                // fillHeart(newcard, docID);
                 //i++;   //Optional: iterate variable to serve as unique ID
             })
         })
