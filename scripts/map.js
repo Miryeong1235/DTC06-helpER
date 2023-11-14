d7b37528bc3eaa95d2de7b157ae1d59005827e51
Eric Kim <hkim467@my.bcit.ca>
2023-11-13 20:03:21 -0800

    Add marker1 to map.js

--- scripts/map.js
+++ scripts/map.js
@@ -10,8 +10,20 @@ function showMap() {
         zoom: 8 // Starting zoom
     });
 
-    // Add user controls to map
-    map.addControl(new mapboxgl.NavigationControl());
+    // Create a default Marker and add it to the map.
+    const marker1 = new mapboxgl.Marker()
+        .setLngLat([49.261529903991004, -123.12220479030786])
+        .addTo(map);
+
+    // Add user controls to map (compass and zoom) to top left
+    var nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'top-left');
+
+    // declare some globally used variables
+    var userLocationMarker;
+    var searchLocationMarker;
+    var userLocation;
+    var searchLocation;
 
     //------------------------------------
     // Listen for when map finishes loading
@@ -127,63 +139,55 @@ function showMap() {
                 // Add the image to the map style with width and height values
                 map.addImage('userpin', image, { width: 10, height: 10 });
 
-                // Adds user's current location as a source to the map
-                navigator.geolocation.getCurrentPosition(position => {
-                    const userLocation = [position.coords.longitude, position.coords.latitude];
+                // Get the user's location
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    userLocation = [position.coords.longitude, position.coords.latitude];
                     console.log(userLocation);
-                    if (userLocation) {
-                        map.addSource('userLocation', {
-                            'type': 'geojson',
-                            'data': {
-                                'type': 'FeatureCollection',
-                                'features': [{
-                                    'type': 'Feature',
-                                    'geometry': {
-                                        'type': 'Point',
-                                        'coordinates': userLocation
-                                    },
-                                    'properties': {
-                                        'description': 'Your location'
-                                    }
-                                }]
-                            }
-                        });
+                    console.log(searchLocation);
 
-                        // Creates a layer above the map displaying the user's location
-                        map.addLayer({
-                            'id': 'userLocation',
-                            'type': 'symbol',
-                            'source': 'userLocation',
-                            'layout': {
-                                'icon-image': 'userpin', // Pin Icon
-                                'icon-size': 0.05, // Pin Size
-                                'icon-allow-overlap': true // Allows icons to overlap
-                            }
-                        });
+                    // Add a marker to the map at the user's location
+                    userLocationMarker = new mapboxgl.Marker()
+                        .setLngLat(userLocation)
+                        .addTo(map);
 
-                        // Map On Click function that creates a popup displaying the user's location
-                        map.on('click', 'userLocation', (e) => {
-                            // Copy coordinates array.
-                            const coordinates = e.features[0].geometry.coordinates.slice();
-                            const description = e.features[0].properties.description;
+                    // Center the map on the user's location
+                    map.flyTo({
+                        center: userLocation
+                    });
+                });
 
-                            new mapboxgl.Popup()
-                                .setLngLat(coordinates)
-                                .setHTML(description)
-                                .addTo(map);
-                        });
+                // Add the MapboxGeocoder search box to the map
+                const geocoder = new MapboxGeocoder({
+                    accessToken: mapboxgl.accessToken,
+                    mapboxgl: mapboxgl
+                });
+                map.addControl(geocoder);
 
-                        // Change the cursor to a pointer when the mouse is over the userLocation layer.
-                        map.on('mouseenter', 'userLocation', () => {
-                            map.getCanvas().style.cursor = 'pointer';
-                        });
+                // Listen for the 'result' event from the geocoder (when a search is made)
+                geocoder.on('result', function (e) {
+                    searchLocation = e.result.geometry.coordinates;
+                    console.log(userLocation);
+                    console.log(searchLocation);
 
-                        // Defaults
-                        // Defaults cursor when not hovering over the userLocation layer
-                        map.on('mouseleave', 'userLocation', () => {
-                            map.getCanvas().style.cursor = '';
-                        });
-                    }
+                    // Add a marker to the map at the search location
+                    searchLocationMarker && searchLocationMarker.remove(); // Remove the previous search marker if it exists
+                    searchLocationMarker = new mapboxgl.Marker({ color: 'red' })
+                        .setLngLat(searchLocation)
+                        .addTo(map);
+
+                    // Fit the map to include both the user's location and the search location
+                    const bounds = new mapboxgl.LngLatBounds();
+                    bounds.extend(userLocation);
+                    bounds.extend(searchLocation);
+
+                    map.fitBounds(bounds, {
+                        padding: {
+                            top: 100,
+                            bottom: 50,
+                            left: 100,
+                            right: 50
+                        } // Add some padding so that markers aren't at the edge or blocked
+                    });
                 });
             }
         );
