2cb5034afa1f203279ec623f846ad98c2dd95ad2
Eric Kim <hkim467@my.bcit.ca>
2023-11-14 13:39:46 -0800

    Debug search.js

--- scripts/search.js
+++ scripts/search.js
@@ -4,39 +4,47 @@ function getQueryParam(param) {
 }
 
 function createQueryFromURL() {
-    const hospitalName = getQueryParam("query");
-    console.log(hospitalName);
-    const hospitalRef = db.collection("hospitals");
-    console.log(hospitalRef);
+    return getQueryParam("query");
 
-    return hospitalRef.where("name", "==", hospitalName);
+    //     const hospitalName = getQueryParam("query");
+    //     console.log(hospitalName);
+    //     const hospitalRef = db.collection("hospitals");
+    //     console.log(hospitalRef);
+
+    //     return hospitalRef.where("name", "==", hospitalName);
 }
 
 // Function to display cards dynamically based on query
 async function displayCardsDynamically(query) {
+
     let cardTemplate = document.getElementById("hospitalCardTemplate");
     console.log(query)
     try {
         // Correct syntax for Firebase SDK 8.10.0
-        const querySnapshot = await query.get();
-        querySnapshot.forEach(doc => {
-            var title = doc.data().name;
-            var details = doc.data().details;
-            var hospitalCode = doc.data().code;
-            var hospitalHour = doc.data().hours;
-            var docID = doc.id;
-            let newcard = cardTemplate.content.cloneNode(true);
+        db.collection('hospitals').get().then((docs) => {
+            docs.forEach(doc => {
+                if (doc.data().name.toLowerCase().includes(query)) {
+                    var title = doc.data().name;
+                    var details = doc.data().details;
+                    var hospitalCode = doc.data().code;
+                    var hospitalHour = doc.data().hours;
+                    var docID = doc.id;
+                    let newcard = cardTemplate.content.cloneNode(true);
 
-            // Assuming fillHeart is a defined function
-            fillHeart(newcard, docID);
-            newcard.querySelector('.card-title').innerHTML = title;
-            newcard.querySelector('.card-hour').innerHTML = hospitalHour;
-            newcard.querySelector('.card-text').innerHTML = details;
-            newcard.querySelector('.card-image').src = `./images/${hospitalCode}.png`;
-            newcard.querySelector('a').href = `hospital_detail.html?docID=${docID}`;
+                    // Assuming fillHeart is a defined function
+                    // fillHeart(newcard, docID);
+                    newcard.querySelector('.card-title').innerHTML = title;
+                    newcard.querySelector('.card-hour').innerHTML = hospitalHour;
+                    newcard.querySelector('.card-text').innerHTML = details;
+                    newcard.querySelector('.card-image').src = `./images/${hospitalCode}.png`;
+                    newcard.querySelector('a').href = `hospital_detail.html?docID=${docID}`;
 
-            document.getElementById("hospitals-go-here").appendChild(newcard);
-        });
+                    document.getElementById("hospitals-go-here").appendChild(newcard);
+                }
+
+            });
+        })
+
     } catch (error) {
         console.error("Error fetching data: ", error);
     }
