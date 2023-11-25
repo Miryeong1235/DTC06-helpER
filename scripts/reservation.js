9136d5c03cd20d07bf42b65eea7325dc03afe2d9
Hsin Pang <hpang19@my.bcit.ca>
2023-11-24 16:44:45 -0800

    show no reservation when there is no reservation

--- scripts/reservation.js
+++ scripts/reservation.js
@@ -25,28 +25,33 @@ function displayCardsDynamically(collection, userUid) { //collection is userProf
     let cardTemplate = document.getElementById('myReservationCardTemplate');
     db.collection(collection).doc(userUid).collection('reservation').get()
         .then(querySnapshot => {
-            querySnapshot.forEach(doc => {
-                let thisReservationID = doc.id; //get reservation id
-                console.log(thisReservationID)
-
-                // get hospital's name based on the reservation ID
-                db.collection('hospitals').doc(thisReservationID).get()
-                    .then(hospital_doc => {
-                        if (hospital_doc.exists) {
-                            let newCard = cardTemplate.content.cloneNode(true);
-                            newCard.querySelector('#currentReservationHospitalName').innerHTML = hospital_doc.data().name;
-                            newCard.querySelector('#currentReservationHospitalId').innerHTML = thisReservationID;
-                            
-                            let randomNumberOfPeople = Math.floor(Math.random() * 30) + 1
-                            newCard.querySelector('#numberOfPeople').innerHTML = randomNumberOfPeople;
-
-                            document.getElementById("current-reservation-go-here").appendChild(newCard);
-
-                        }
-                    })
-
-
-            });
+            if (querySnapshot.docs.length > 0) {
+                querySnapshot.forEach(doc => {
+                    let thisReservationID = doc.id; //get reservation id
+                    console.log(thisReservationID)
+    
+                    // get hospital's name based on the reservation ID
+                    db.collection('hospitals').doc(thisReservationID).get()
+                        .then(hospital_doc => {
+                            if (hospital_doc.exists) {
+                                let newCard = cardTemplate.content.cloneNode(true);
+                                newCard.querySelector('#currentReservationHospitalName').innerHTML = hospital_doc.data().name;
+                                newCard.querySelector('#currentReservationHospitalId').innerHTML = thisReservationID;
+                                
+                                let randomNumberOfPeople = Math.floor(Math.random() * 30) + 1
+                                newCard.querySelector('#numberOfPeople').innerHTML = randomNumberOfPeople;
+    
+                                document.getElementById("current-reservation-go-here").appendChild(newCard);
+    
+                            }
+                        })
+    
+    
+                });
+            } else {
+                document.getElementById("current-reservation-go-here").innerHTML += '<p class="text-center">You have no record of joined waitlist at the moment!</p>';
+            }
+            
         });
 }
 
