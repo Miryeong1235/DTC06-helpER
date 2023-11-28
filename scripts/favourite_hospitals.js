90d496a6aee439fbc47d1255ac7ee8040cec2994
Hsin Pang <hpang19@my.bcit.ca>
2023-11-28 14:29:44 -0800

    add documentation in favourite_hospitals.js and clean up code

--- scripts/favourite_hospitals.js
+++ scripts/favourite_hospitals.js
@@ -1,16 +1,18 @@
-
-var hospitalIdList = undefined;
-
+// initialize userUid as global variables
 var userUid = undefined;
 
+// update userUid in the global variable and get display name from the user Auth to use in html
 function getUserId() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
         if (user) {
             userUid = user.uid;
             userName = user.displayName;
+
+            // display user's name in html
             $("#name-goes-here").text(userName);
 
+            // reflect user's bookmark record
             getBookmarks();
         } else {
             console.log('User not logged in')
@@ -18,14 +20,13 @@ function getUserId() {
     })
 }
 
-function setup() {
-    getUserId();
-}
-
-
+// reflect user's bookmark preference from records in firestore
 function getBookmarks() {
+    // Retrieve user document from userProfiles collection in firestore
     let currentUser = db.collection("userProfiles").doc(userUid);
+    
     currentUser.get().then(userDoc => {
+        // execute if the user document exists in the firestore
         if (userDoc.exists) {
             // Get the Array of bookmarks
             var bookmarks = userDoc.data().bookmarks;
@@ -53,7 +54,7 @@ function getBookmarks() {
                         newcard.querySelector('i').id = "heart-" + thisHospitalID;
                         newcard.querySelector('i').onclick = () => updateBookmark(thisHospitalID);
 
-                        //Finally, attach this new card to the gallery
+                        //Finally, attach this new card to the favorite hospital list
                         document.getElementById("favourite-go-here").appendChild(newcard);
 
                         // update the bookmark if the hospital is saved as favourite for the current user
@@ -64,14 +65,19 @@ function getBookmarks() {
                     })
                 })
             } else {
+                // Display a message if there is no hospital newcard information appended
                 if (document.getElementById("favourite-go-here").innerHTML.trim() == '') {
                     document.getElementById("favourite-go-here").innerHTML += '<p class="py-5 my-5 text-secondary">You have not saved any hospital yet!</p>';
                 }
             }
-
-
         };
     })
+}
+
+// the setup function when this js script is referenced
+function setup() {
+    // update the userUid gobal variable at the beginning
+    getUserId();
 }
 
 $(document).ready(setup)
