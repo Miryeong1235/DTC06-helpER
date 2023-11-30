2c918f765ecf84b26da414cfd0b879bcf1e2c49b
Hsin Pang <hpang19@my.bcit.ca>
2023-11-30 11:39:37 -0800

    add documentation for my_review.js

--- scripts/my_review.js
+++ scripts/my_review.js
@@ -1,9 +1,13 @@
+// declare userUid as global variable
 var userUid = undefined;
-var favouriteDocRef = undefined;
 
+//-------------------------------
+// Get name and id from user Auth
+//-------------------------------
 function getUserId() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
+        // if true, call displayCardsDynamically
         if (user) {
             var userUid = user.uid;
             displayCardsDynamically("review", userUid);
@@ -13,6 +17,7 @@ function getUserId() {
     })
 }
 
+// update userUid global variable when the script loaded
 getUserId();
 
 
@@ -20,36 +25,34 @@ getUserId();
 // Input parameter is a string representing the collection we are reading from
 //------------------------------------------------------------------------------
 function displayCardsDynamically(collection, userUid) {
-    
 
-    let cardTemplate = document.getElementById("myReviewTemplate"); // Retrieve the HTML element with the ID "hospitalCardTemplate" and store it in the cardTemplate variable. 
+    // Retrieve the HTML element with the ID "hospitalCardTemplate" and store it in the cardTemplate variable. 
+    let cardTemplate = document.getElementById("myReviewTemplate");
 
-    db.collection(collection).get()   //the collection called "review"
+    //the collection called "review"
+    db.collection(collection).get()
         .then(allReviews => {
-            allReviews.forEach(doc => { //iterate thru each doc
+            //iterate thru each review doc
+            allReviews.forEach(doc => {
                 let doc_id = doc.id
+                // since the doc is is composed of hospital id + user id with "-" in between
+                // here we want to search for the doc match with the userUid and add hospital card with this matched portion
                 if (doc_id.startsWith(userUid)) {
+                    // get the hospital id from the doc id after "-" character
                     let hospitalId = doc_id.split('-')[1];
+
+                    // get the doc of selected hospital in hospitals collection
                     db.collection("hospitals").doc(hospitalId).get().then(hospital_doc => {
-                        console.log(doc.data(), '====', hospital_doc.data());
+                        // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
                         let newcard = cardTemplate.content.cloneNode(true);
+                        // Extract number of star in integer
                         let rating = parseInt(doc.data().rating.split(' stars')[0]);
+                        //update title and review content
                         newcard.querySelector('#reviewHospitalName').innerHTML = hospital_doc.data().name;
                         newcard.querySelector('#reviewStar').innerHTML = '★'.repeat(rating) + '☆'.repeat(5 - rating);;
                         newcard.querySelector('#reviewContent').innerHTML = doc.data().comment;
                         document.getElementById(collection + "-go-here").appendChild(newcard);
                     })
-                    // title = hospitalName;
-                    // var rating = parseInt(doc.data().rating.split(' stars')[0]);
-                    // var stars = '★'.repeat(rating) + '☆'.repeat(5 - rating);
-                    // var comments = doc.data().comment;
-                    // let newcard = cardTemplate.content.cloneNode(true);
-                    //update title and text and image
-                    // newcard.querySelector('#reviewHospitalName').innerHTML = hospitalName;
-                    // newcard.querySelector('#reviewStar').innerHTML = stars;
-                    // newcard.querySelector('#reviewContent').innerHTML = comments;
-
-                    // document.getElementById(collection + "-go-here").appendChild(newcard);
                 }
 
             })
